9b393e1604ced224a8e28a8d9d3175960a180373
Cheng XU <git@xuc.me>
2022-02-21 15:39:36 -0800

    update script

--- scripts/json_to_csv.py
+++ scripts/json_to_csv.py
@@ -7,6 +7,7 @@ import sys
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print(f"{sys.argv[0]} <input.json> <output.csv>")
+        sys.exit(1)
 
     df = pd.read_json(sys.argv[1])
     df.to_csv(sys.argv[2], index=False)
