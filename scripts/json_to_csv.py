0766df69fd265131afe047d9a25ced0793dcda43
Cheng XU <git@xuc.me>
2022-02-21 00:03:20 -0800

    convert json to csv

---
+++ scripts/json_to_csv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print(f"{sys.argv[0]} <input.json> <output.csv>")
+
+    df = pd.read_json(sys.argv[1])
+    df.to_csv(sys.argv[2], index=False)
+
