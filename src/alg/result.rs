23960c40983b22eeff29bc5454d782b702a9e121
Cheng XU <git@xuc.me>
2022-02-21 14:37:27 -0800

    human time

--- src/alg/result.rs
+++ src/alg/result.rs
@@ -4,7 +4,23 @@ use std::{collections::HashMap, time::Duration};
 
 #[derive(Debug, Default, Clone, Serialize, Deserialize)]
 pub struct ShapleyResult {
+    #[serde(with = "serde_time")]
     pub avg_time: Duration,
+    #[serde(with = "serde_time")]
     pub total_time: Duration,
     pub shapley_values: HashMap<SellerId, f64>,
+}
+
+mod serde_time {
+    use super::*;
+    use serde::{de::Deserializer, ser::Serializer};
+
+    pub fn serialize<S: Serializer>(t: &Duration, s: S) -> Result<S::Ok, S::Error> {
+        s.serialize_f64(t.as_secs_f64())
+    }
+
+    pub fn deserialize<'de, D: Deserializer<'de>>(d: D) -> Result<Duration, D::Error> {
+        let t = <f64>::deserialize(d)?;
+        Ok(Duration::from_secs_f64(t))
+    }
 }
